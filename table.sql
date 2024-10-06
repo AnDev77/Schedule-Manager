ed544c5c04427979455550ab6c24969aa08fbcb2
AnDev77 <dnr0627@naver.com>
2024-10-07 03:08:01 +0900

    table.sql 추가

---
+++ table.sql
@@ -0,0 +1,59 @@
+CREATE TABLE users (
+
+	id INT AUTO_INCREMENT PRIMARY KEY,
+
+	email VARCHAR(45) NOT NULL,
+
+	password VARCHAR(45) NOT NULL
+
+);
+
+
+CREATE TABLE teams (
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+name VARCHAR(45) NOT NULL
+
+);
+
+CREATE TABLE TeamMembers (
+
+	id INT AUTO_INCREMENT PRIMARY KEY,
+
+	user_id INT,
+
+	team_id INT,
+
+	FOREIGN KEY (user_id) REFERENCES users(id),
+
+	FOREIGN KEY (team_id) REFERENCES teams(id)
+);
+
+
+CREATE TABLE schedules (
+
+	id INT AUTO_INCREMENT PRIMARY KEY,
+
+	user_id INT,
+
+	team_id INT DEFAULT NULL,
+
+	title VARCHAR(45) NOT NULL,
+
+	detail TEXT,
+
+	start_date DATE NOT NULL,
+    
+    start_time TIME,
+	
+    end_time	TIME,
+
+	end_date DATE NOT NULL,
+
+	repeat_type ENUM('NONE', 'DAILY', 'WEEKLY', 'MONTHLY') NOT NULL,
+
+	FOREIGN KEY (user_id) REFERENCES users(id),
+
+	FOREIGN KEY (team_id) REFERENCES teams(id)
+);
