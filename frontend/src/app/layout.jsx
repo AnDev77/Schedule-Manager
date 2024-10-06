ab82b51c0fec7d46b0866121d6e59cce1974359b
Lee Taehoon <gb4tt3ry@gmail.com>
2024-10-07 07:25:56 +0900

    Remove: Wanted Sans 제거

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -2,15 +2,6 @@ import localFont from "next/font/local";
 import { paperlogy, uhBeeZziba } from "@/fonts";
 import "@/styles/globals.css";
 
-// const wantedSans = localFont({
-//     src: "../fonts/WantedSansVariable.woff2",
-//     preload: true,
-//     style: "normal",
-//     display: "swap",
-//     weight: "400 1000",
-//     variable: "--font-wanted-sans",
-// });
-
 export const metadata = {
     title: "Managers",
     description: "일정 관리 프로젝트",
