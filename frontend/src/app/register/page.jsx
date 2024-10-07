0729d0c88937f0f677f2cf6174a8f2a79fd5c381
Lee-chaerin <5543705@naver.com>
2024-10-08 04:25:58 +0900

    Feat: 회원가입 페이지 추가

--- frontend/src/app/register/page.jsx
+++ frontend/src/app/register/page.jsx
@@ -1,5 +1,31 @@
+"use client"
+
+import InputBox from '@/components/common/input-box';
+import BigButton from '@/components/common/big-button';
+import Title from '@/components/common/title';
+import styles from '@/styles/pages/login.module.css';
+import { useState } from 'react';
+
 const Register = () => {
     // TODO: 회원가입 페이지 구현
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordCheck, setPasswordCheck] = useState('');
+        
+    const handleClick = (e) => {
+        alert(`Id: ${id}, PW: ${password}, PW 확인: ${passwordCheck}`)
+    }
+
+    return (
+        <>
+            <Title>Schedule<br/>Manager</Title>
+            <InputBox placeholder='ID' value={id} onChange={(e) => setId(e.target.value)} />
+            <InputBox type='password' placeholder='PASSWORD' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <InputBox type='password' placeholder='PASSWORD 확인' value={passwordCheck} onChange={(e) => setPasswordCheck(e.target.value)} />
+            <br/>
+            <BigButton className={styles.loginButton} onClick={handleClick}>회원가입</BigButton>
+        </>
+    )
 }
 
 export default Register;
