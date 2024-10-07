bba2a988e7714e62f09ee7aafcb374e1f409cfee
znhxxn <ghoon7777@naver.com>
2024-10-07 22:32:28 +0900

    Chore: mariadb.js에 env 활용

--- backend/src/mariadb.js
+++ backend/src/mariadb.js
@@ -1,12 +1,14 @@
+require('dotenv').config(); // dotenv 모듈 소환
+
 // mysql 모듈 소환
 const mariadb = require('mysql2');
 
 // db와 연결 통로 생성
 const connection = mariadb.createConnection({
-    host: '',
-    user: '',
-    password: '',
-    database : '', 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     dateStrings: true
 });
 
