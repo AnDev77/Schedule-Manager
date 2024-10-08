404700337366afb5f3da879e2c49d84dcfa1cfbb
znhxxn <ghoon7777@naver.com>
2024-10-08 21:28:58 +0900

    Feat: user.js, UserController 틀 구현

--- backend/src/controller/UserController.js
+++ backend/src/controller/UserController.js
@@ -1,3 +1,25 @@
-const conn = require('../mariadb'); // mariadb 연결 모듈 
+const conn = require('../mariadb'); 
+const { StatusCodes } = require('http-status-codes'); 
 
-module.exports = {};
+const join = function(req, res) {
+    res.status(StatusCodes.OK).send("회원가입"); 
+};
+
+const login = function(req, res) {
+    res.status(StatusCodes.OK).send("로그인"); 
+};
+
+const idCheck = function(req, res) {
+    res.status(StatusCodes.OK).send("아이디 확인"); 
+};
+
+const passwordReset = function(req, res) {
+    res.status(StatusCodes.OK).send("비밀번호 변경"); 
+};
+
+module.exports = {
+    join, 
+    login, 
+    idCheck, 
+    passwordReset 
+};
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,5 +1,14 @@
 const express = require('express'); // express 모듈
 const router = express.Router();
-const {} = require('../controller/UserController');
+const {join, login, idCheck, passwordReset} = require('../controller/UserController');
+router.use(express.json());
+
+router.post('/join', join); // 회원 가입
+
+router.post('/login', login); // 로그인
+
+router.post('/idcheck', idCheck); // 아이디 확인
+
+router.put('/reset', passwordReset); // 비밀번호 변경
 
 module.exports = router
