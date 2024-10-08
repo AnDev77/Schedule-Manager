965af7a851270ee392364587f8773377b6dc3a4e
Lee Taehoon <gb4tt3ry@gmail.com>
2024-10-09 00:26:29 +0900

    Feat: FullCalendar 테스트용 추가

--- frontend/src/app/calendar/page.jsx
+++ frontend/src/app/calendar/page.jsx
@@ -1,5 +1,32 @@
+'use client'
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+const events = [
+    { title: 'Meeting1', start: new Date(), allDay: true, },
+    { title: 'Meeting2', start: new Date(), allDay: true, },
+    { title: 'Meeting3', start: new Date(), allDay: true, },
+    { title: 'Meeting4', start: new Date(), allDay: true, },
+]
+
 const CalendarMain = () => {
     // TODO: 캘린더 페이지 구현
+    return (
+        <div>
+            <FullCalendar
+                plugins={[ dayGridPlugin ]}
+                initialView='dayGridMonth'
+                events={events}
+                height={'100vh'}
+                locale={'ko'}
+                moreLinkText={(n) => `+${n} 더보기`}
+                dayMaxEvents
+                nowIndicator
+                selectable
+            />
+        </div>
+    )
 }
 
 export default CalendarMain;
