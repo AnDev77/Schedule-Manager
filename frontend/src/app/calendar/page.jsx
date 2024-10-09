66c5028fb281658cc1bab930822a7b4a784bd475
Lee Taehoon <gb4tt3ry@gmail.com>
2024-10-09 18:59:25 +0900

    Feat: FullCalender 사용해 캘린더 구현 (#19)
    * Feat: FullCalendar 테스트용 추가
    
    * Feat: 한글화, 리다이렉트, 커스텀 버튼 구현

--- frontend/src/app/calendar/page.jsx
+++ frontend/src/app/calendar/page.jsx
@@ -1,5 +1,53 @@
+'use client'
+
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { useRouter } from 'next/navigation';
+
+const events = [
+    { id: 1, title: 'Meeting1', start: new Date(), allDay: true, },
+    { id: 2, title: 'Meeting2', start: new Date(), allDay: true, },
+    { id: 3, title: 'Meeting3', start: new Date(), allDay: true, },
+    { id: 4, title: 'Meeting4', start: new Date(), allDay: true, },
+]
+
 const CalendarMain = () => {
     // TODO: 캘린더 페이지 구현
+    const router = useRouter();
+
+    return (
+        <div>
+            <FullCalendar
+                plugins={[ dayGridPlugin ]}
+                initialView='dayGridMonth'
+                events={events}
+                height={'100vh'}
+                locale={'ko'}
+                headerToolbar={{
+                    right: 'customAdd today prev,next'
+                }}
+                buttonText={{
+                    today: '오늘',
+                }}
+                buttonHints={{
+                    today: '오늘 날짜로 이동',
+                    prev: '지난 달로 이동',
+                    next: '다음 달로 이동',
+                }}
+                customButtons={{
+                    customAdd: {
+                        text: '새 일정',
+                        hint: '새 일정 추가',
+                        click: () => { router.push('/calendar/new') },
+                    }
+                }}
+                moreLinkText={(n) => `+${n} 더보기`}
+                eventClick={({ event }) => router.push(`/calendar/${event.id}`)}
+                dayMaxEvents
+                nowIndicator
+            />
+        </div>
+    )
 }
 
 export default CalendarMain;
