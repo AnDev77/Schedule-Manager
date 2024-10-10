d20b8b45273cab8efe4a58620126ec44fec73e5c
Lee-chaerin <5543705@naver.com>
2024-10-10 15:37:45 +0900

    Feat: ScheduleInputBox 컴포넌트 추가

---
+++ frontend/src/components/common/schedule-input-box.jsx
@@ -0,0 +1,36 @@
+import styles from '@/styles/common/schedule-input-box.module.css';
+
+import RemoveList from '@heroicons/react/24/solid/MinusCircleIcon';
+import UserPlus from '@heroicons/react/24/solid/UserplusIcon';
+
+const ScheduleInputBox = ({
+    type = 'text',
+    placeholder = '',
+    value,
+    onChange,
+    className = '',
+    onRemove, 
+    onUserPlusClick,
+    ...props
+}) => {
+    return (
+        <div className={styles.div}> 
+            <RemoveList className={styles.list} onClick={onRemove}/>
+            <div className={styles.inputDiv}>   
+                <input
+                    type={type}
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={onChange}
+                    className={`${styles.scheduleInputBox} ${className}`}
+                    {...props}
+                />
+                <UserPlus className={styles.icons} onClick={onUserPlusClick}/>
+            </div>
+        </div>
+    );
+};
+
+ScheduleInputBox.dipslayName = 'ScheduleInputBox';
+
+export default ScheduleInputBox;
