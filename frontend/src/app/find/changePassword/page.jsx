dba0383a5e1eb0739fc778a6f04c6a5717717848
Lee Taehoon <gb4tt3ry@gmail.com>
2024-10-11 02:24:26 +0900

    Feat: react-hook-form 사용

--- frontend/src/app/find/changePassword/page.jsx
+++ frontend/src/app/find/changePassword/page.jsx
@@ -4,32 +4,49 @@ import InputBox from '@/components/common/input-box';
 import BigButton from '@/components/common/big-button';
 import Title from '@/components/common/title';
 import styles from '@/styles/pages/login.module.css';
-import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
 
 const ChangePassword = () => {
     // TODO: 유저 설정 페이지(비밀번호 변경) 구현
-    const [password, setPassword] = useState('');
-    const [passwordCheck, setPasswordCheck] = useState('');
     const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
         
-    const handleClick = (e) => {
-        if(password !== passwordCheck) {
+    const onSubmit = (data) => {
+        if(data.password !== data.passwordCheck) {
             alert('비밀번호를 확인해주세요.');
-        } else {
-            alert('비밀번호가 변경되었습니다.');
-            router.push('/login');
+            return;
         }
+        alert(JSON.stringify(data, null, 4));
+        router.push('/login');
     }
 
     return (
-        <>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <Title>Schedule<br/>Manager</Title>
-            <InputBox type='password' placeholder='PASSWORD' value={password} onChange={(e) => setPassword(e.target.value)} />
-            <InputBox type='password' placeholder='PASSWORD 확인' value={passwordCheck} onChange={(e) => setPasswordCheck(e.target.value)} />
+            <div className={styles.inputContainer}>
+                <InputBox
+                    {...register('password', { required: true })}
+                    type='password'
+                    placeholder='PASSWORD'
+                />
+                {errors.password && <p>비밀번호 입력 필수</p>}
+            </div>
+            <div className={styles.inputContainer}>
+                <InputBox
+                    {...register('passwordCheck', { required: true })}
+                    type='password'
+                    placeholder='PASSWORD 확인'
+                />
+                {errors.passwordCheck && <p>비밀번호 입력 필수</p>}
+            </div>
             <br/>
-            <BigButton className={styles.loginButton} onClick={handleClick}>비밀번호 변경</BigButton>
-        </>
+            <BigButton type='submit' className={styles.loginButton}>비밀번호 변경</BigButton>
+        </form>
     )
 }
 
