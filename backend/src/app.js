098cf0ee4e1edf6b3be7fa17efc7b7c560d12bef
Lee Taehoon <gb4tt3ry@gmail.com>
2024-10-12 23:33:03 +0900

    Feat: 프론트 통신을 위한 cors 세팅 (#28)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // dotenv 모듈
@@ -6,6 +7,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 app.listen(process.env.PORT); // 실제 프로젝트 시 유의미한 port 번호 지정
+
+app.use(cors({
+    origin: 'http://localhost:3001', // 프론트엔드 주소
+    credentials: true,
+    exposedHeaders: ['Set-Cookie', 'Date', 'ETag']
+}));
 
 // 라우팅 기능
 const userRouter = require('./routes/users');
