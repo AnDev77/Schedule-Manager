4d9b2033927ef929689684ee00553a6f85264479
Lee Taehoon <gb4tt3ry@gmail.com>
2024-10-14 02:27:07 +0900

    Feat: useUser 함수 수정

--- frontend/src/data/use-user.js
+++ frontend/src/data/use-user.js
@@ -2,9 +2,6 @@ import { useState, useEffect, useMemo } from 'react';
 import { jwtDecode } from 'jwt-decode';
 
 const getJwt = () => {
-    if (typeof window === 'undefined') {
-        return null;
-    }
     const match = document.cookie.match(/(?:^|;)\s*token=([^;]*)/);
     return match ? decodeURIComponent(match[1]) : null;
 }
@@ -12,14 +9,12 @@ const getJwt = () => {
 const useUser = () => {
     const [user, setUser] = useState({ id: null, email: null });
 
-    const jwt = useMemo(() => getJwt(), []);
-
     useEffect(() => {
+        const jwt = getJwt();
         if (!jwt) return;
-
         const { id, email } = jwtDecode(jwt);
         setUser({ id, email });
-    }, [jwt]);
+    }, []);
 
     return user;
 }
