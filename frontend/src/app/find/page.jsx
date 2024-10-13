3d62c031bb282eda0e5a6e19b0c1fc3ac287542d
Lee Taehoon <gb4tt3ry@gmail.com>
2024-10-13 23:29:25 +0900

    Feat: 아이디 찾기 페이지 기능 구현

--- frontend/src/app/find/page.jsx
+++ frontend/src/app/find/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import InputBox from '@/components/common/input-box';
 import BigButton from '@/components/common/big-button';
@@ -8,7 +8,6 @@ import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 
 const Find = () => {
-    // TODO: 유저 설정 페이지(아이디 확인) 구현
     const router = useRouter();
     const {
         register,
@@ -16,8 +15,27 @@ const Find = () => {
         formState: { errors },
     } = useForm();
         
-    const onSubmit = (data) => {
-        alert(JSON.stringify(data, null, 4));
+    const onSubmit = async (data) => {
+        const response = await fetch('http://localhost:3000/users/idcheck', {
+            mode: 'cors',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+            credentials: 'include',
+        });
+
+        if (response.status == 400) {
+            alert('오류가 발생하였습니다.');
+            return;
+        }
+
+        if (response.status == 401) {
+            alert('존재하지 않는 계정입니다.');
+            return;
+        }
+        
         router.push('/find/changePassword');
     }
 
@@ -26,13 +44,13 @@ const Find = () => {
             <Title>Schedule<br/>Manager</Title>
             <div className={styles.inputContainer}>
                 <InputBox
-                    {...register('id', { required: true })}
-                    placeholder='ID'
+                    {...register('email', { required: true })}
+                    placeholder='EMAIL'
                 />
-                {errors.id && <p>아이디 입력 필수</p>}
+                {errors.email && <p>이메일 입력 필수</p>}
             </div>
             <br/>
-            <BigButton type='submit' className={styles.loginButton}>아이디 확인</BigButton>
+            <BigButton type='submit' className={styles.loginButton}>계정 확인</BigButton>
         </form>
     )
 }
