2364df4282c711738926b0364c5ef964410ce16b
Lee Taehoon <gb4tt3ry@gmail.com>
2024-10-13 23:58:51 +0900

    Feat: Link 추가 (#39)

--- frontend/src/components/calendar/header.jsx
+++ frontend/src/components/calendar/header.jsx
@@ -4,6 +4,7 @@ import styles from '@/styles/calendar/header.module.css';
 
 import Bell from '@heroicons/react/24/solid/BellIcon';
 import User from '@heroicons/react/24/solid/UserIcon';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 export default function Header({
@@ -18,9 +19,9 @@ export default function Header({
     return (
         <div className={styles.div}>
             <header className={styles.header}>
-                <Bell className={styles.icons}/>
-                <h1 className={styles.title}>Schedule Manager</h1>
-                <User className={styles.icons} onClick={handleUserPage}/>
+                <Link href='/alert'><Bell className={styles.icons}/></Link>
+                <Link href='/'><h1 className={styles.title}>Schedule Manager</h1></Link>
+                <Link href='/user'><User className={styles.icons} onClick={handleUserPage}/></Link>
             </header>
             <main className={styles.main}>
                 {children}
