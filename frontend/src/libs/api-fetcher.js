c8e32be4d2c44e1ba1afbc50a28aed46ff9224c1
Lee Taehoon <gb4tt3ry@gmail.com>
2024-10-14 02:59:15 +0900

    Remove: api-fetcher.js

--- frontend/src/libs/api-fetcher.js
+++
@@ -1,7 +0,0 @@
-const apiFetcher = async (path) => {
-    const response = await fetch(`http://localhost:3000${path}`, { mode: 'no-cors' });
-    const json = await response.json();
-    return json;
-};
-
-export default apiFetcher;
